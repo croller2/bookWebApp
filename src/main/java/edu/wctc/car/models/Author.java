d7ed507521e57ebedbae4202872d2688c4a56fe9
croller2 <croller2@my.wctc.edu>
2017-03-29 12:42:31 -0500

    Added new table and book entity, updated author entity with FK -> PK

--- src/main/java/edu/wctc/car/models/Author.java
+++ src/main/java/edu/wctc/car/models/Author.java
@@ -1,8 +1,8 @@
-
 package edu.wctc.car.models;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Set;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -11,11 +11,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
@@ -30,6 +32,9 @@ import javax.xml.bind.annotation.XmlRootElement;
     , @NamedQuery(name = "Author.findByAuthorName", query = "SELECT a FROM Author a WHERE a.authorName = :authorName")
     , @NamedQuery(name = "Author.findByDateAdded", query = "SELECT a FROM Author a WHERE a.dateAdded = :dateAdded")})
 public class Author implements Serializable {
+
+    @OneToMany(mappedBy = "authorEntity")
+    private Set<Book> bookSet;
 
     private static final long serialVersionUID = 1L;
     @Id
@@ -99,5 +104,14 @@ public class Author implements Serializable {
     public String toString() {
         return "edu.wctc.car.models.Author[ authorId=" + authorId + " ]";
     }
-    
+
+    @XmlTransient
+    public Set<Book> getBookSet() {
+        return bookSet;
+    }
+
+    public void setBookSet(Set<Book> bookSet) {
+        this.bookSet = bookSet;
+    }
+
 }
